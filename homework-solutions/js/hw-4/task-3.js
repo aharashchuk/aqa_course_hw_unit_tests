a6c92297ce4fd334e5c2a6690346210524848c7d
aharashchuk <a.harashchuk@gmail.com>
2025-07-29 21:27:55 +0200

    hw4 tasks 1-3

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -1,8 +1,20 @@
-/*
+"use strict";
 
-(НЕ ОБЯЗАТЕЛЬНО)
+const minAge = 18;
+const maxAge = 60;
+let age = "17";
+console.log(`age: ${age}`);
 
-Преобразовать Task 2 таким образом, чтобы значение НАПРИМЕР '2' (т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, 
-  преобразовываясь в number
+// Преобразовать Task 2 таким образом, чтобы значение НАПРИМЕР '2' (т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, 
+// преобразовываясь в number
+age = Number(age);
 
-*/
+if (age < minAge ) {
+  console.log(`You don't have access cause your age is ${age}. It's less then ${minAge}.`);
+ } else if (age >= minAge && age < maxAge) {
+  console.log("Welcome!");
+ } else if (age > maxAge) {
+  console.log("Keep calm and look Culture channel.");
+ } else {
+  console.log("Technical work");
+ }
